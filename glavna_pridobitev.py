e1dbfb488e9f58b0d6cd396a30304dacae07e26b
Valerij15 <valerij.val@gmail.com>
2022-02-12 22:39:34 +0100

    Pridobljene še podrobne informacije o lokalih.

--- glavna_pridobitev.py
+++ glavna_pridobitev.py
@@ -1,5 +1,7 @@
 import requests
 import os
+import csv
+import time
 
 
 glavna_stran_url = 'https://www.studentska-prehrana.si/sl/restaurant'
@@ -28,6 +30,30 @@ def shrani_v_datoteko(besedilo, mapa, ime_datoteke):
     with open(pot, 'w', encoding='utf-8') as izhodna_datoteka:
         izhodna_datoteka.write(besedilo)
 
+def preberi_id_lokalov(mapa, ime_datoteke):
+    id_lokalov = []
+    os.makedirs(mapa, exist_ok=True)
+    pot = os.path.join(mapa, ime_datoteke)
+    with open(pot, 'r', encoding='utf-8') as csv_file:
+        bralec = csv.DictReader(csv_file)
+        for vrstica in bralec:
+            id_lokalov.append(vrstica['id'])
+    return id_lokalov
 
-besedilo = prenesi_iz_url(glavna_stran_url)
-shrani_v_datoteko(besedilo, mapa, ime_datoteke)
+def preberi_podrobne_informacije(datoteka, num = 20):
+    id_lokalov = preberi_id_lokalov('csv_datoteke', datoteka)
+    n = 0
+    i = 1
+    for id in id_lokalov:
+       print(f'{glavna_stran_url}/Details/{id}')
+       besedilo = prenesi_iz_url(f'{glavna_stran_url}/Details/{id}')
+       n += 1
+       shrani_v_datoteko(besedilo, mapa, f'podrobne_informacije{i}.html')
+       if n >= 20:
+        n = 0
+        i += 1
+            
+
+#preberi_podrobne_informacije('lokali.csv', 20)
+#besedilo = prenesi_iz_url(glavna_stran_url)
+#shrani_v_datoteko(besedilo, mapa, ime_datoteke)
