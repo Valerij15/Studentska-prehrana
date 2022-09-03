df04c7c3dd6355947cb6b99ca956ba35e06ae5f5
Valerij15 <valerij.val@gmail.com>
2022-09-03 23:48:52 +0200

    Popravljena obdelava podatkov.

--- glavna_obdelava.py
+++ glavna_obdelava.py
@@ -15,7 +15,7 @@ def stran_v_lokale(stran):
     return re.findall(narejen_vzorec, stran)
 
 def pridobi_podatke_lokala(lokal):
-    vzorec = r'data-cena="(?P<cena>.*?)".*?data-doplacilo="(?P<doplacilo>.*?)".*?data-posid="(?P<id>.*?)".*?data-lokal="(?P<ime>.*?)".*?data-city="(?P<mesto>.*?)".*?'
+    vzorec = r'data-naslov="(?P<naslov>.*?)".*?data-doplacilo="(?P<doplacilo>.*?)".*?data-posid="(?P<id>.*?)".*?data-lokal="(?P<ime>.*?)".*?data-city="(?P<mesto>.*?)".*?'
     narejen_vzorec = re.compile(vzorec, re.DOTALL)
     najdeno = re.search(narejen_vzorec, lokal)
     if najdeno:
@@ -34,9 +34,10 @@ def napisi_csv(polja, vrstice, mapa, ime_datoteke):
     os.makedirs(mapa, exist_ok=True)
     pot = os.path.join(mapa, ime_datoteke)
     with open(pot, 'w', encoding='utf-8') as csv_file:
-        pisar = csv.DictWriter(csv_file, fieldnames=polja, extrasaction='ignore')
+        pisar = csv.DictWriter(csv_file, fieldnames=polja, extrasaction='ignore',delimiter = ";")
         pisar.writeheader()
         for vrstica in vrstice:
+            vrstica['ime'] = vrstica['ime'].replace('&quot;','').replace('&amp;', '&')
             pisar.writerow(vrstica)
 
 
@@ -51,5 +52,5 @@ for lokal in seznam:
     if slovar is not None:
         seznam_ocen.append(slovar)
 
-napisi_csv(['id', 'ime', 'cena', 'doplacilo','mesto'], seznam_podatkov, 'csv_datoteke', 'lokali.csv')
+napisi_csv(['id', 'ime', 'naslov', 'doplacilo','mesto'], seznam_podatkov, 'csv_datoteke', 'lokali.csv')
 napisi_csv(['id', 'ime', 'ocena'], seznam_ocen, 'csv_datoteke', 'ocene.csv')
